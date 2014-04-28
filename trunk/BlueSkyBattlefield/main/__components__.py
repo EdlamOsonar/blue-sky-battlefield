ad711905f193e9b75d090a23798a0e5d20083886
famalde@gmail.com <famalde@gmail.com@85a7d80d-a86e-6484-6669-d26e61c3f2c3>
2014-04-28 14:17:08 +0000

    Disparo nave del heroe

--- trunk/BlueSkyBattlefield/main/__components__.py
+++ trunk/BlueSkyBattlefield/main/__components__.py
@@ -53,8 +53,20 @@ class NaveHeroe(Nave):
     velocidadMovimiento = 15
     
     def postConstructor(self):
-        self.disparando = False
-        self.disparos = [Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5)]
+        self.numeroDisparos =0
+        
+        #crear aray de disparos
+        self.disparos = [Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5),
+                             Disparo(self.screen, self.image_dir, self.file_name_image_disparo, 3, 5)]
          
     def moverIzquierda(self):
         self.moverX(-self.velocidadMovimiento)
@@ -63,13 +75,22 @@ class NaveHeroe(Nave):
         self.moverX(self.velocidadMovimiento)
         
     def disparar(self):        
-        self.disparando = True
-        for i in xrange(len(self.disparos)):
-            self.disparos[i].pintar(self.posicionX , self.posicionY)
-    
+        if self.numeroDisparos < len(self.disparos):
+            disparo = self.disparos[self.numeroDisparos]            
+            disparo.pintar(self.posicionX , self.posicionY)                
+            self.numeroDisparos = self.numeroDisparos + 1           
+        
+            
     def updateDisparos(self):
-        for i in xrange(len(self.disparos)):
+        for i in xrange(self.numeroDisparos):
             self.disparos[i].update()
+            
+            #comprobar si los disparos ya no estan en pantalla para disminuir el contao de disparos (solo puede haber self.numeroDisparos en pantalla)
+            if self.disparos[i].posicionY < 0:
+                self.disparos[i].posicionX = self.posicionX
+                self.disparos[i].posicionY = self.posicionY
+                self.numeroDisparos = self.numeroDisparos - 1
+                #print("disparo -> " + str(i) + " posicion en pantalla en el eje y " + str(self.disparos[i].posicionY))
 
 class NaveEnemiga(Nave):
       
@@ -86,13 +107,13 @@ class Disparo(pygame.sprite.Sprite):
         self.posicionX = 0
         self.posicionY = 0       
         
-    def pintar(self, initPosicionX, initPosicionY):
+    def pintar(self, initPosicionX, initPosicionY):        
         self.posicionX = initPosicionX
         self.posicionY = initPosicionY
         self.screen.blit(self.scaledImage, (self.posicionX, self.posicionY))
     
     def update(self):
         self.posicionY = self.posicionY -10
-        self.screen.blit(self.scaledImage, (self.posicionX + 5, self.posicionY - 10))
+        self.screen.blit(self.scaledImage, (self.posicionX, self.posicionY))
             
         
